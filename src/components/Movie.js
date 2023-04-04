d9f71434e79d6db7e8b0ec32c3425bb7c3fd5d10
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-04-04 13:46:45 -0500

    mentor session

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+//import MovieList from './MovieList';
 
 export default class Movie extends React.Component{
 //constructor for movie data
-  constructor(props){
-    super(props);
-    this.state = {
-        id: props.id,
-        title: props.title,
-        image: props.image,
-        synopsis: props.synopsis,
-        rating: props.rating
-    };
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //       id: props.id,
+  //       title: props.title,
+  //       image: props.image,
+  //       synopsis: props.synopsis,
+  //       rating: props.rating
+  //   };
+  // }
 
     render(){
+      console.log(this.props.title);
         return(
             <Card className='movieCard' style={{ width: '18rem' }}>
       <Card.Img className="moviePoster" variant="top" src={this.state.image} height="200px" />
